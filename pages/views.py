ee892cfb0009d28561e6de921a0fe59a692d49c2
Mariam <Mariamhh98@hotmail.com>
2019-08-21 16:01:35 +0300

    Mariam and Bashayer

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Page
+
 
 # Create your views here.
+def page_list(request):
+    
+    context = {
+            "page_list":  Page.objects.all(),
+            }
+    return render(request, "list.html", context)
+
+
+
+def page_detail(request, page_id):
+    
+    context = {
+            "page_detail":  Page.objects.get(id = page_id)
+            }
+    
+    return render(request, "detail.html", context)
